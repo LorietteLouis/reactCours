98853aa1ebc9ebab87f25b7ac93381fd499be8cc
Louis Loriette <louis.loriette@lapiscine.pro>
2023-07-10 17:23:43 +0200

    mise à jour hook et usestate

--- coworkings/src/Allcoworkings.js
+++ coworkings/src/Allcoworkings.js
@@ -1,13 +1,28 @@
-
+import {useState} from "react";
 import CoworkingShow from "./CoworkingShow";
 
 const AllCoworkings = ({coworkingsFromFakeApi}) =>{
+    const [displayCoworkings, setDisplayCoworkings] = useState(false)
+
+    const handleClick = () => {
+        setDisplayCoworkings(!displayCoworkings)
+      };
+
     return(
         <div className="AllPlaces">
+        <div className= "btn">   
+            <button onClick={handleClick}>Afficher les coworkings</button>
+        </div>
+        {displayCoworkings && (
+            <div className="coworkeAll">
             {coworkingsFromFakeApi.map(coworking =>(
                 <CoworkingShow coworking={coworking}/>
+
             ))}
+            </div>
+        )}
         </div>
+        
     )
 }
 
